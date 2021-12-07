79cd562927642a2cdf98daea6d506a0232a5ce06
Andres Rodriguez <kflex25@MacBook-Pro-de-Andres.local>
2021-12-07 14:07:30 -0600

    Survey

---
+++ static/js/index.js
@@ -0,0 +1,29 @@
+console.log("Server On");
+
+let socket = io("http://localhost:8080");
+
+
+$('.forminfo').on('submit', function(event){
+    event.preventDefault();
+
+    let namey = $('#name').val();
+    let location = $('#location').val();
+    let language = $('#language').val();
+    let comment = $('#comment').val();
+    let number = Math.floor(Math.random()*1000);
+
+    let info={
+        name: namey,
+        location: location,
+        language: language,
+        comment: comment,
+        number: number
+    };
+    
+    socket.emit('information', info);
+});
+
+socket.on('display', function(info){
+    let message = `<p> You emited the following information to the server: Name: ${info.name}, Location: ${info.location}, Favorite language: ${info.language} and Comment: ${info.comment} Your lucky number emited by the server is: ${info.number} </p>`;
+    $('.message' ).append(message);
+});
